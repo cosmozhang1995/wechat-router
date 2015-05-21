67360bceaa2eb3881e9693ee8779b86594d88fe6
cosmozhang <zjzxz3006@126.com>
2015-05-21 10:50:43 +0800

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -12,34 +12,39 @@ router.get('/', function(req, res) {
 });
 
 for (var name in configs.platforms) {
-	var theRoute = "" + name;
-	router.get('/' + theRoute, function(req, res) {
-		var search = "?signature=" + req["signature"]
-			+ "&timestamp=" + req["timestamp"]
-			+ "&nonce=" + req["nonce"]
-			+ req["echostr"] ? ("&echostr=" + req["echostr"]) : "";
-		request.get(configs.platforms[theRoute], function(error, response, body) {
-			res.send(body);
-		});
-	});
-	router.post('/' + theRoute, function(req, res) {
-		var search = "?signature=" + req["signature"]
-			+ "&timestamp=" + req["timestamp"]
-			+ "&nonce=" + req["nonce"]
-			+ req["echostr"] ? ("&echostr=" + req["echostr"]) : "";
-		getBody(req, {
-			limit: '100kb',
-			length: req.headers['content-length'],
-			encoding: 'utf8'
-		}, function(err, buf) {
-			request.post({
-				url: configs.platforms[theRoute],
-				formData: buf
-			}, function(error, response, body) {
-				if (body) res.send(body);
-			});
-		});
-	});
+        var theRoute = "" + name;
+        var url = configs.platforms[theRoute];
+        router.get('/' + theRoute, (function(url) {
+            return function(req, res) {
+                var search = "?signature=" + req["signature"]
+                        + "&timestamp=" + req["timestamp"]
+                        + "&nonce=" + req["nonce"]
+                        + req["echostr"] ? ("&echostr=" + req["echostr"]) : "";
+                request.get(url + search, function(error, response, body) {
+                        res.send(body);
+                });
+            }
+        })(url));
+        router.post('/' + theRoute, function(url) {
+        	return function(req, res) {
+                var search = "?signature=" + req["signature"]
+                        + "&timestamp=" + req["timestamp"]
+                        + "&nonce=" + req["nonce"]
+                        + req["echostr"] ? ("&echostr=" + req["echostr"]) : "";
+                getBody(req, {
+                        limit: '100kb',
+                        length: req.headers['content-length'],
+                        encoding: 'utf8'
+                }, function(err, buf) {
+                        request.post({
+                                url: url + search,
+                                formData: buf
+                        }, function(error, response, body) {
+                                if (body) res.send(body);
+                        });
+                });
+	        }
+        });
 }
 
 module.exports = router;
