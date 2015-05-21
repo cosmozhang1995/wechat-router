ffa80f21ad7dcb12d5295a53700cd824431cfbaa
cosmozhang <zjzxz3006@126.com>
2015-05-21 10:14:33 +0800

    first commit

---
+++ routes/configs.js
@@ -0,0 +1,6 @@
+module.exports = {
+	platforms: {
+		"notifier22": "http://localhost:8031/wechat",
+		"dengdengdengdeng": "http://localhost:8032/wechat"
+	}
+}
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var request = require('request');
+var getBody = require('raw-body');
+
+var router = express.Router();
+
+var configs = require('./configs');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+for (var name in configs.platforms) {
+	var search = "?signature=" + req["signature"]
+		+ "&timestamp=" + req["timestamp"]
+		+ "&nonce=" + req["nonce"]
+		+ req["echostr"] ? ("&echostr=" + req["echostr"]) : "";
+	router.get('/' + name, function(req, res) {
+		request.get(configs.platforms[name], function(error, response, body) {
+			res.send(body);
+		});
+	});
+	router.post('/' + name, function(req, res) {
+		getBody(req, {
+			limit: '100kb',
+			length: req.headers['content-length'],
+			encoding: 'utf8'
+		}, function(err, buf) {
+			request.post({
+				url: configs.platforms[name],
+				formData: buf
+			}, function(error, response, body) {
+				if (body) res.send(body);
+			});
+		});
+	});
+}
+
+module.exports = router;
